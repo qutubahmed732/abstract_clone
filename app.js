861ccdf96bcd78ebff8ae575767bd633d377abbb
qutub ahmed <qutubahmed104@gmail.com>
2024-12-24 13:34:42 +0500

    tablet responsive

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const button = document.querySelector(".sign-in");
+const menu_button = document.querySelector(".fa-bars");
+const hidden_buttons = document.querySelector(".hidden-buttons");
 
-button.addEventListener("keydown", ()=>{
-    if(event.key){
-        event.preventDefault()
-        alert(event.key + ' pressed');
+if (document.body.style.width >= '1000px') {
+    hidden_buttons.style.display = 'none'
+}
+
+menu_button.addEventListener("click", () => {
+    if (hidden_buttons.style.display == 'none') {
+        hidden_buttons.style.display = 'flex';
+    } else {
+        hidden_buttons.style.display = 'none';
     }
-});
+})
+
+console.log(document.body.style)
